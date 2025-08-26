c30e029c3be4ddcc20a0b4109fc49e2c8973cd5d
Zhivko Shopov <zhishopov@gmail.com>
2025-08-26 16:26:16 +0100

    Lifting state

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState("");
-
-  function handleCLick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleCLick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquareClick={handleClick}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
       </div>
     </>
   );
