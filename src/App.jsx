fd9700fac80ec9edde1c7d6ddccda93f33dbdc75
Zhivko Shopov <zhishopov@gmail.com>
2025-08-26 16:41:00 +0100

    Calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function Board() {
   const [isNext, setIsNext] = useState(true);
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -73,4 +73,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
 }
