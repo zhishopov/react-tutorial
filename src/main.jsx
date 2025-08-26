935c9cdfde77c3a17e8edd12fb66c356fad51549
Zhivko Shopov <zhishopov@gmail.com>
2025-08-26 16:09:30 +0100

    Create Square component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App.jsx";
+import Board from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<Board />);
